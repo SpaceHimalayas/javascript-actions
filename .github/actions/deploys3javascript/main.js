fe6e460b9d04766bc43cfbeb7b55811a0b45af10
SpaceHimalayas <spacehimalayas3@gmail.com>
2026-06-04 11:31:01 +0000

    new

--- .github/actions/deploys3javascript/main.js
+++ .github/actions/deploys3javascript/main.js
@@ -1,8 +1,15 @@
-const core = require('@actions/core')
-const github = require('@actions/github')
-const exec = require('@actions/exec')
+const core = require('@actions/core');
+//const github = require('@actions/github');
+const exec = require('@actions/exec');
 
 function run() {
-    core.notice('Hello from my custom javascript action')
+    const bucket = core.getInput('bucket', { required: true });
+    const bucketRegion = core.getInput('bucket-region', { required: false });
+    const distFolder = core.getInput('dist-folder', { required: true });
+
+    const s3Url = `s3://${bucket}`
+    exec.exec(`aws s3 sync ${distFolder} ${s3Url} --region ${bucketRegion}`)
+
+    core.notice('Hello from my custom javascript action');
 }
 run();
